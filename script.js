57cb995ef7260b631ca42880a89001212f7118a2
HaDQ <ha4656629@gmail.com>
2026-01-17 17:49:41 +0700

    Improve UI

--- script.js
+++ script.js
@@ -328,6 +328,18 @@ function render() {
     songTitle.className = "song-title";
     songTitle.textContent = s.title;
     
+    // Add playing animation if this is the currently playing song
+    if (i === currentIndex && !audio.paused) {
+      const playingAnimation = document.createElement("div");
+      playingAnimation.className = "playing-animation";
+      for (let j = 0; j < 4; j++) {
+        const bar = document.createElement("span");
+        bar.style.animationDelay = `${j * 0.2}s`;
+        playingAnimation.appendChild(bar);
+      }
+      songTitle.appendChild(playingAnimation);
+    }
+    
     // Song artist
     const songArtist = document.createElement("span");
     songArtist.className = "song-artist";
@@ -458,6 +470,7 @@ playBtn.onclick = () => {
         <path d="M2.7 1a.7.7 0 00-.7.7v12.6a.7.7 0 00.7.7h2.6a.7.7 0 00.7-.7V1.7a.7.7 0 00-.7-.7H2.7zm8 0a.7.7 0 00-.7.7v12.6a.7.7 0 00.7.7h2.6a.7.7 0 00.7-.7V1.7a.7.7 0 00-.7-.7h-2.6z"/>
       </svg>
     `;
+    render(); // Update playing animation
   } else {
     audio.pause();
     playBtn.innerHTML = `
@@ -465,6 +478,7 @@ playBtn.onclick = () => {
         <path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"/>
       </svg>
     `;
+    render(); // Update playing animation
   }
 };
 
